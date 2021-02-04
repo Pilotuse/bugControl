33035f0ccd6ed35d893ef26dd6a83b60e71721b5
fanhongyu <fanhongyu126@126.com>
2021-02-05 01:44:22 +0800

    提交注册代码

--- src/router/index.js
+++ src/router/index.js
@@ -8,20 +8,32 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta:{
+      title:'首页'
+    }
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/Login.vue'),
+    meta:{
+      title:'登录'
+    }
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to,from,next)=>{
+    if(to?.meta?.title){
+      document.title = to.meta.title
+      next()
+    }else {
+      next()
+    }
 })
 
 export default router
