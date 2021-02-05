e2fbf72671d831d0bbe636fc960d75f710846559
茶都醉何况酒 <5350654+Nostalgiz@user.noreply.gitee.com>
2021-02-05 10:28:35 +0800

    提交登录页面代码

--- src/router/index.js
+++ src/router/index.js
@@ -9,16 +9,16 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
-    meta:{
-      title:'首页'
+    meta: {
+      title: '首页'
     }
   },
   {
     path: '/login',
     name: 'login',
     component: () => import('../views/Login.vue'),
-    meta:{
-      title:'登录'
+    meta: {
+      title: 'Quality Center-登录'
     }
   }
 ]
@@ -27,13 +27,13 @@ const router = new VueRouter({
   routes
 })
 
-router.beforeEach((to,from,next)=>{
-    if(to?.meta?.title){
-      document.title = to.meta.title
-      next()
-    }else {
-      next()
-    }
+router.beforeEach((to, from, next) => {
+  if (to?.meta?.title) {
+    document.title = to.meta.title
+    next()
+  } else {
+    next()
+  }
 })
 
 export default router
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,88 +1,97 @@
 <template>
-    <div class="container">
+  <div class="container clearfix">
+    <div class="display-box">
+      <div class="left">
+        <div class="desc">质量管理中心</div>
+        <div class="enc">Quality Center</div>
+
+      </div>
+      <div class="right clearfix">
         <div class="login" v-if="displayBox">
-            <h2>登录</h2>
-            <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="60px" class="demo-ruleForm">
-                <el-form-item label="用户名" prop="pass">
-                    <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
-                </el-form-item>
-                <el-form-item label="密码" prop="checkPass">
-                    <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
-                </el-form-item>
-                <el-form-item>
-                    <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
-                </el-form-item>
-            </el-form>
+          <h2>登录</h2>
+          <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="60px" class="demo-ruleForm">
+            <el-form-item label="用户名" prop="pass">
+              <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="密码" prop="checkPass">
+              <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+            </el-form-item>
+          </el-form>
         </div>
-        <div class="register" v-else>注册</div>
+        <div class="register" v-else></div>
+      </div>
     </div>
+  </div>
 </template>
 
 
 <script>
 export default {
-    data() {
-        var checkAge = (rule, value, callback) => {
-            if (!value) {
-                return callback(new Error("年龄不能为空"));
-            }
-            setTimeout(() => {
-                if (!Number.isInteger(value)) {
-                    callback(new Error("请输入数字值"));
-                } else {
-                    if (value < 18) {
-                        callback(new Error("必须年满18岁"));
-                    } else {
-                        callback();
-                    }
-                }
-            }, 1000);
-        };
-        var validatePass = (rule, value, callback) => {
-            if (value === "") {
-                callback(new Error("请输入密码"));
-            } else {
-                if (this.ruleForm.checkPass !== "") {
-                    this.$refs.ruleForm.validateField("checkPass");
-                }
-                callback();
-            }
-        };
-        var validatePass2 = (rule, value, callback) => {
-            if (value === "") {
-                callback(new Error("请再次输入密码"));
-            } else if (value !== this.ruleForm.pass) {
-                callback(new Error("两次输入密码不一致!"));
-            } else {
-                callback();
-            }
-        };
-        return {
-            displayBox: true,
-            ruleForm: {
-                pass: "",
-                checkPass: "",
-                age: "",
-            },
-            rules: {
-                pass: [{ validator: validatePass, trigger: "blur" }],
-                checkPass: [{ validator: validatePass2, trigger: "blur" }],
-                age: [{ validator: checkAge, trigger: "blur" }],
-            },
-        };
+  data() {
+    var checkAge = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error("年龄不能为空"));
+      }
+      setTimeout(() => {
+        if (!Number.isInteger(value)) {
+          callback(new Error("请输入数字值"));
+        } else {
+          if (value < 18) {
+            callback(new Error("必须年满18岁"));
+          } else {
+            callback();
+          }
+        }
+      }, 1000);
+    };
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      } else {
+        if (this.ruleForm.checkPass !== "") {
+          this.$refs.ruleForm.validateField("checkPass");
+        }
+        callback();
+      }
+    };
+    var validatePass2 = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value !== this.ruleForm.pass) {
+        callback(new Error("两次输入密码不一致!"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      displayBox: true,
+      ruleForm: {
+        pass: "",
+        checkPass: "",
+        age: "",
+      },
+      rules: {
+        pass: [{ validator: validatePass, trigger: "blur" }],
+        checkPass: [{ validator: validatePass2, trigger: "blur" }],
+        age: [{ validator: checkAge, trigger: "blur" }],
+      },
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
     },
-    methods: {
-        submitForm(formName) {
-            this.$refs[formName].validate((valid) => {
-                if (valid) {
-                    alert("submit!");
-                } else {
-                    console.log("error submit!!");
-                    return false;
-                }
-            });
-        },
-    },
+  },
 };
 </script>
 
@@ -90,22 +99,50 @@ export default {
 
 <style scoped>
 .container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100vw;
-    height: 100vh;
-    background-image: url("../assets/img/MojaveNight.jpg");
-    background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-image: url("../assets/img/MojaveNight.jpg");
+  background-size: cover;
+}
+
+.display-box {
+  display: flex;
+  padding: 20px;
+  min-width: 40vw;
+  min-width: 50vw;
+  min-height: 60vh;
+  background: #fff;
+  box-sizing: border-box;
+  border-radius: 8px;
+}
+
+.display-box > .right,
+.display-box > .left {
+  flex: 1 !important;
+}
+
+.left {
+  min-height: 60vh;
+  background: #fff;
+  float: left;
+  box-sizing: border-box;
+  background-image: url("../assets/img/bgimg.svg");
+  background-repeat: no-repeat;
+  background-size: 400px;
+  background-position: 60px 80px;
 }
 
 .login,
 .register {
-    padding: 20px;
-    width: 400px;
-    min-height: 400px;
-    background: #f9f9f9;
-    border-radius: 10px;
-    box-shadow: 0 0 8px 1px #384d6b;
+  padding: 20px;
+  float: right;
+  width: 80%;
+  min-height: 60vh;
+  box-sizing: border-box;
+  border-radius: 8px;
+  overflow: hidden;
 }
 </style>
