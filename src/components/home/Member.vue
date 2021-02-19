cb7205bfbbcdc361ef14392a24700418900bd632
xiaowang <1056312367@163.com>
2021-02-19 23:26:35 +0800

    ‘工作台

--- src/components/home/Member.vue
+++ src/components/home/Member.vue
@@ -1,5 +1,56 @@
 <template>
-    <div>
-        成员
+  <div>
+    <leaveadd></leaveadd>
+    <div class="list">
+      <el-table :data="leaveList" border style="width: 100%">
+        <el-table-column fixed prop="leaveReason" label="待完成">
+        </el-table-column>
+        <el-table-column label="开始时间">
+          <template slot-scope="scope">
+            <span>{{ scope.row.startDate | filterDate }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="结束时间">
+          <template slot-scope="scope">
+            <span>{{ scope.row.endDate | filterDate }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="附件">
+          <template slot-scope="scope">
+            <span v-if="scope.row.pic">
+              <el-image
+                style="width: 100px; height: 100px"
+                :src="scope.row.pic.split('_')[0]"
+                :preview-src-list="scope.row.pic.split('_')"
+              >
+              </el-image>
+            </span>
+          </template>
+        </el-table-column>
+
+        <el-table-column fixed="right" label="操作">
+          <template slot-scope="scope">
+            <el-button type="primary" v-if="scope.row.type == 1"
+              >待修复</el-button
+            >
+            <el-button type="success" v-else-if="scope.row.type == 2"
+              >已修复</el-button
+            >
+          </template>
+        </el-table-column>
+      </el-table>
     </div>
+  </div>
 </template>
+<script>
+import { mapState, mapActions } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(["leaveList"]),
+  },
+  methods: {
+    ...mapActions(["playload"]),
+  },
+};
+</script>
