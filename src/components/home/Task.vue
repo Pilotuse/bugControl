384a37e47069ef7af6ec0d96d49d731da00bdc01
fanhongyu <fanhongyu126@126.com>
2021-02-19 22:53:28 +0800

    修改代码

--- src/components/home/Task.vue
+++ src/components/home/Task.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
-        任务模块
+        <tester></tester>
     </div>
 </template>
+
+
+<script>
+import tester from '../role/Tester'
+
+export default {
+    components:{
+        tester
+    }
+}
+</script>
