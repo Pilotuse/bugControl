66fcc82523aafe084518121c2f141da9a539c8c4
小王 <1056312367@163.com>
2021-02-21 14:06:34 +0800

    1111

--- src/module/developer.js
+++ src/module/developer.js
@@ -2,20 +2,7 @@ import { post } from './methods'
 
 export default {
     // 用户权限查询接口 ，返回用户菜单
-    tester: ({
-        taskname,
-        taskdetail,
-        degree,
-        tasklabel,
-        taskperson,
-        cutoffday,
-        status
-    }) => post('/api/user/transaction', {
-        taskname,
-        taskdetail,
-        degree,
-        tasklabel,
-        taskperson,
-        cutoffday, status, apiName: 'queryBugOrder', bipcode: 'BIP10010', activitycode: 'ACT10010'
+    tester: () => post('/api/user/transaction', {
+        apiName: 'queryBugOrder', bipcode: 'BIP10010', activitycode: 'ACT10010'
     }),
 }
