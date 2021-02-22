99dcd424d46090d3f9abe1ffc39d1412202af169
panyan520 <email@example.com>
2021-02-22 19:37:25 +0800

    asd

--- src/components/role/Developer.vue
+++ src/components/role/Developer.vue
@@ -3,11 +3,15 @@
     <h3>概览报表</h3>
     <el-card class="box-card box">
       <div class="text">
-        问题总数 <p>{{ tableall }}</p>
+        问题总数
+        <p>{{ tableall }}</p>
       </div>
-      <div class="text"> 已完成 <p>{{ tablejie }}</p>
+      <div class="text">
+        已完成
+        <p>{{ tablejie }}</p>
       </div>
-      <div class="text1"> 未完成
+      <div class="text1">
+        未完成
         <p>{{ tableall - tablejie }}</p>
       </div>
       <div id="myecharts1"></div>
@@ -27,13 +31,21 @@
           <el-button type="danger">重要</el-button>
         </el-table-column>
 
-        <el-table-column prop="end_time" label="日期" sortable width="180" column-key="date">
+        <el-table-column
+          prop="end_time"
+          label="日期"
+          sortable
+          width="180"
+          column-key="date"
+        >
         </el-table-column>
         <el-table-column prop="details" label="任务详情" width="300">
         </el-table-column>
         <el-table-column label="操作" width="180">
           <template slot-scope="scope">
-            <el-button type="primary" v-if="scope.row.type == 1">已修复</el-button>
+            <el-button type="primary" v-if="scope.row.type == 1"
+              >已修复</el-button
+            >
             <el-button type="dangers" v-else>待修复</el-button>
           </template>
         </el-table-column>
@@ -55,42 +67,9 @@ export default {
   },
   methods: {
     ...mapActions(["queryBugOrder", "queryUser"]),
-    queryBugInfo() {
-      let that = this;
-      this.queryBugOrder({
-        callback({
-          params: {
-            result: { msg },
-          },
-        }) {
-          if (typeof msg == "object") {
-            that.tableall = msg.length;
-            // console.log(msg.length);
-
-            msg.forEach((el) => {
-              that.tableData.push({
-                idnumber: el.id,
-                belongto: el.belongto,
-                label: el.label,
-                degree: el.degree,
-                end_time: that.dayjs(el.end_time).format("YYYY-MM-DD"),
-                details: el.details,
-                status: el.status,
-              });
-            });
-      }
-          console.log(that.tableData);
-      let data = that.tableData.filter((el) => {
-        return el.status == "1";
-      });
-      console.log(data.length);
-      that.tablejie = data.length;
-    },
-  });
-    },
   },
   created() {
-    this.queryBugInfo();
+    // this.queryBugInfo();
   },
   tableRowClassName({ rowIndex }) {
     if (rowIndex === 0) {
@@ -102,147 +81,142 @@ export default {
   },
 
   mounted() {
-    var myChart = echarts.init(document.getElementById("myecharts"));
-    var myChart1 = echarts.init(document.getElementById("myecharts1"));
-    var option = {
-      tooltip: {
-        trigger: "item",
-      },
-      legend: {
-        top: "5%",
-        left: "center",
-      },
-      series: [
-        {
-          type: "pie",
-          radius: ["50%", "60%"],
-          avoidLabelOverlap: false,
-          label: {
-            show: false,
-            position: "center",
-          },
-          emphasis: {
-            label: {
-              show: true,
-              fontSize: "30",
-              fontWeight: "bold",
-            },
-          },
-          labelLine: {
-            show: false,
-          },
-          data: [
-            { value: 1, name: "未完成" },
-
-            { value: 4, name: "已完成" },
-          ],
+    let that = this;
+    this.queryBugOrder({
+      callback({
+        params: {
+          result: { msg },
         },
-      ],
-    };
-    var option1 = {
-      tooltip: {
-        trigger: "item",
-      },
-      legend: {
-        top: "5%",
-        left: "center",
-      },
-      series: [
-        {
-          type: "pie",
-          radius: ["50%", "60%"],
-          avoidLabelOverlap: false,
-          label: {
-            show: false,
-            position: "center",
-          },
-          emphasis: {
-            label: {
-              show: true,
-              fontSize: "30",
-              fontWeight: "bold",
-            },
-          },
-          labelLine: {
-            show: false,
-          },
-          data: [
-            { value: 9, name: "未完成" },
+      }) {
+        if (typeof msg == "object") {
+          that.tableall = msg.length;
+          // console.log(msg.length);
 
-            { value: 1, name: "已完成" },
+          msg.forEach((el) => {
+            that.tableData.push({
+              idnumber: el.id,
+              belongto: el.belongto,
+              label: el.label,
+              degree: el.degree,
+              end_time: that.dayjs(el.end_time).format("YYYY-MM-DD"),
+              details: el.details,
+              status: el.status,
+            });
+          });
+        }
+        console.log(that.tableData);
+        let data = that.tableData.filter((el) => {
+          return el.status == "1";
+        });
+        console.log(data.length);
+        that.tablejie = data.length;
+
+        var myChart = echarts.init(document.getElementById("myecharts"));
+        var option = {
+          tooltip: {
+            trigger: "item",
+          },
+          legend: {
+            top: "5%",
+            left: "center",
+          },
+          series: [
+            {
+              type: "pie",
+              radius: ["50%", "60%"],
+              avoidLabelOverlap: false,
+              label: {
+                show: false,
+                position: "center",
+              },
+              emphasis: {
+                label: {
+                  show: true,
+                  fontSize: "30",
+                  fontWeight: "bold",
+                },
+              },
+              labelLine: {
+                show: false,
+              },
+              data: [
+                { value: that.tableall - that.tablejie, name: "未完成" },
+
+                { value: that.tablejie, name: "已完成" },
+              ],
+            },
           ],
-        },
-      ],
-    };
-    option1 && myChart1.setOption(option1);
-    option && myChart.setOption(option);
+        };
+        option && myChart.setOption(option);
+      },
+    });
   },
 };
 </script>
 
 <style scoped>
-  .table {
-    padding: 24px;
-  }
+.table {
+  padding: 24px;
+}
 
-  .el-table .warning-row {
-    background: oldlace;
-  }
+.el-table .warning-row {
+  background: oldlace;
+}
 
-  .el-table .success-row {
-    background: #f0f9eb;
-  }
+.el-table .success-row {
+  background: #f0f9eb;
+}
 
-  .box-card {
-    width: 100%;
-  }
+.box-card {
+  width: 100%;
+}
 
-  .box {
-    height: 300px;
-    position: relative;
-  }
+.box {
+  height: 300px;
+  position: relative;
+}
 
-  #myecharts {
-    width: 300px;
-    height: 100%;
-    position: absolute;
-    right: 0;
-    top: 0;
-    border-left: 1px solid #3333;
-  }
+#myecharts {
+  width: 300px;
+  height: 100%;
+  position: absolute;
+  right: 0;
+  top: 0;
+  border-left: 1px solid #3333;
+}
 
-  #myecharts1 {
-    width: 300px;
-    height: 100%;
-    position: absolute;
-    right: 300px;
-    top: 0;
-    border-left: 1px solid #3333;
-  }
+#myecharts1 {
+  width: 300px;
+  height: 100%;
+  position: absolute;
+  right: 300px;
+  top: 0;
+  border-left: 1px solid #3333;
+}
 
-  .text,
-  .text1 {
-    width: 150px;
-    text-align: center;
-    font-size: 20px;
-    font-weight: 600;
-    float: left;
-    margin-top: 80px;
-  }
+.text,
+.text1 {
+  width: 150px;
+  text-align: center;
+  font-size: 20px;
+  font-weight: 600;
+  float: left;
+  margin-top: 80px;
+}
 
-  .text1 {
-    float: left;
-  }
+.text1 {
+  float: left;
+}
 
-  .text p {
-    color: green;
-    font-size: 30px;
-    font-weight: 700;
-  }
+.text p {
+  color: green;
+  font-size: 30px;
+  font-weight: 700;
+}
 
-  .text1 p {
-    color: red;
-    font-size: 30px;
-    font-weight: 700;
-  }
+.text1 p {
+  color: red;
+  font-size: 30px;
+  font-weight: 700;
+}
 </style>
