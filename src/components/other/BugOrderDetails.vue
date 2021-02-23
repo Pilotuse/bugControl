c6c5e0d5a95e816371a66dec8e19ee9d8e9befa7
茶都醉何况酒 <5350654+Nostalgiz@user.noreply.gitee.com>
2021-02-23 14:39:32 +0800

    ...

--- src/components/other/BugOrderDetails.vue
+++ src/components/other/BugOrderDetails.vue
@@ -26,15 +26,27 @@
 
       <el-dropdown @command="issueDegreeCall">
         <el-button :style="{color:issueDegreeDescriber.style}" style="font-size:12px;">
-          <i class="iconfont icon-yuan" style="margin-right:8px;font-size:12px;" :style="issueDegreeDescriber.style"></i> {{issueDegreeDescriber.describer}}
+          <i class="iconfont icon-tag" style="margin-right:8px;font-size:12px;" :style="issueDegreeDescriber.style"></i> {{issueDegreeDescriber.describer}}
         </el-button>
         <el-dropdown-menu slot="dropdown">
           <el-dropdown-item :command="item.command" v-for="item in issueDegree" :key="item.id" :style="{color:item.style}" style="font-size:12px;">
-            <i class="iconfont icon-yuan" style="margin-right:8px;font-size:12px;" :style="item.style"></i> {{item.describer}}
+            <i class="iconfont icon-tag" style="margin-right:8px;font-size:12px;" :style="item.style"></i> {{item.describer}}
           </el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
 
+    </div>
+    <el-divider></el-divider>
+
+    <h4 class="title">任务信息</h4>
+    <div class="issue-info">
+      <span class="issue-id">#CQVC1</span>
+      <span class="create-time">创建于 2020-02-23</span>
+    </div>
+
+    <div class="issue-describer" @dblclick="showMarkBox">
+      <span v-if="!markdownContainer">暂无数据</span>
+      <markdown v-if="markdownContainer"></markdown>
     </div>
     <el-divider></el-divider>
 
@@ -43,6 +55,7 @@
 
 
 <script>
+import markdown from './Markdown'
 export default {
   data() {
     return {
@@ -55,12 +68,14 @@ export default {
       ],
       issueStateGet: {},
       issueDegree: [
-        { id: 1, command: 'serious', describer: '严重', style: '#FE811A', status: false },
-        { id: 2, command: 'high', describer: '高', style: '#5798E4', status: false },
-        { id: 3, command: 'middle', describer: '中', style: '#FEC018', status: false },
-        { id: 4, command: 'low', describer: '低', style: '#F56C6C', status: false },
+        { id: 1, command: 'serious', describer: '严重', style: '#EF0016', status: false },
+        { id: 2, command: 'high', describer: '高', style: '#FF8F00', status: false },
+        { id: 3, command: 'middle', describer: '中', style: '#008AFF', status: false },
+        { id: 4, command: 'low', describer: '低', style: '#73E06F', status: false },
+        { id: 5, command: 'nan', describer: '无优先级', style: '#B0B0B0', status: false },
       ],
-      issueDegreeDescriber: {}
+      issueDegreeDescriber: {},
+      markdownContainer: false
     }
   },
   props: ["bugordershow"],
@@ -73,7 +88,14 @@ export default {
     },
     issueDegreeCall(command) {
       this.issueDegree.forEach(el => el.status = command == el.command ? true : false)
+    },
+    showMarkBox() {
+      console.log(111);
+      this.markdownContainer = true
     }
+  },
+  components: {
+    markdown
   },
   watch: {
     issueState: {
@@ -113,6 +135,10 @@ export default {
   color: #ccc;
 }
 
+.bug-pack-up i {
+  font-size: 12px;
+}
+
 .bug-header button {
   line-height: 20px;
   padding: 5px 10px;
@@ -130,5 +156,35 @@ export default {
 
 .el-dropdown {
   margin-right: 6px;
+}
+
+h4.title {
+  margin: 20px 0 10px 0;
+  font-size: 16px;
+  color: rgba(0, 0, 0, 0.8);
+}
+
+.issue-info .issue-id {
+  display: inline-block;
+  width: 53px;
+  height: 18px;
+  background: #f5f6f8;
+  color: #8c92a4;
+  border-radius: 4px;
+  line-height: 18px;
+  font-size: 12px;
+  cursor: pointer;
+  text-align: center;
+  margin: 0 6px 0 0;
+}
+
+.issue-info .create-time {
+  font-size: 12px;
+  color: #8c92a4;
+}
+
+.issue-describer {
+  margin: 10px 0 20px 0;
+  font-size: 14px;
 }
 </style>
